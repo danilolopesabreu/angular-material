4b820981b352405f8592e78f7153ad0f44e2ed5c
danilolopesabreu <danilo.bcc@gmail.com>
2021-12-21 14:52:17 -0300

    v1

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,215 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { TableData } from '../md/md-table/md-table.component';
+import { LegendItem, ChartType } from '../md/md-chart/md-chart.component';
+
+import * as Chartist from 'chartist';
+
+declare var $: any;
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html'
+})
+export class DashboardComponent implements OnInit, AfterViewInit {
+  // constructor(private navbarTitleService: NavbarTitleService, private notificationService: NotificationService) { }
+  public tableData: TableData;
+  startAnimationForLineChart(chart: any) {
+      let seq: any, delays: any, durations: any;
+      seq = 0;
+      delays = 80;
+      durations = 500;
+      chart.on('draw', function(data: any) {
+
+        if (data.type === 'line' || data.type === 'area') {
+          data.element.animate({
+            d: {
+              begin: 600,
+              dur: 700,
+              from: data.path.clone().scale(1, 0).translate(0, data.chartRect.height()).stringify(),
+              to: data.path.clone().stringify(),
+              easing: Chartist.Svg.Easing.easeOutQuint
+            }
+          });
+        } else if (data.type === 'point') {
+              seq++;
+              data.element.animate({
+                opacity: {
+                  begin: seq * delays,
+                  dur: durations,
+                  from: 0,
+                  to: 1,
+                  easing: 'ease'
+                }
+              });
+          }
+      });
+
+      seq = 0;
+  }
+  startAnimationForBarChart(chart: any) {
+      let seq2: any, delays2: any, durations2: any;
+      seq2 = 0;
+      delays2 = 80;
+      durations2 = 500;
+      chart.on('draw', function(data: any) {
+        if (data.type === 'bar') {
+            seq2++;
+            data.element.animate({
+              opacity: {
+                begin: seq2 * delays2,
+                dur: durations2,
+                from: 0,
+                to: 1,
+                easing: 'ease'
+              }
+            });
+        }
+      });
+
+      seq2 = 0;
+  }
+  // constructor(private navbarTitleService: NavbarTitleService) { }
+  public ngOnInit() {
+      this.tableData = {
+          headerRow: ['ID', 'Name', 'Salary', 'Country', 'City'],
+          dataRows: [
+              ['US', 'USA', '2.920	', '53.23%'],
+              ['DE', 'Germany', '1.300', '20.43%'],
+              ['AU', 'Australia', '760', '10.35%'],
+              ['GB', 'United Kingdom	', '690', '7.87%'],
+              ['RO', 'Romania', '600', '5.94%'],
+              ['BR', 'Brasil', '550', '4.34%']
+          ]
+       };
+      /* ----------==========     Daily Sales Chart initialization    ==========---------- */
+
+      const dataDailySalesChart = {
+          labels: ['S', 'T', 'Q', 'Q', 'S', 'S', 'D'],
+          series: [
+              [12, 15, 7, 9, 13, 10, 3]
+          ]
+      };
+
+     const optionsDailySalesChart = {
+          lineSmooth: Chartist.Interpolation.cardinal({
+              tension: 0
+          }),
+          low: 0,
+          high: 20, // creative tim: we recommend you to set the high sa the biggest value + something for a better look
+          chartPadding: { top: 0, right: 0, bottom: 0, left: 0},
+      };
+
+      const dailySalesChart = new Chartist.Line('#dailySalesChart', dataDailySalesChart, optionsDailySalesChart);
+
+      this.startAnimationForLineChart(dailySalesChart);
+      /* ----------==========     Completed Tasks Chart initialization    ==========---------- */
+
+      const dataCompletedTasksChart = {
+          labels: ['S', 'T', 'Q', 'Q', 'S', 'S', 'D'],
+          series: [
+              [12, 7, 14, 3, 11, 9, 10]
+          ]
+      };
+
+      const optionsCompletedTasksChart = {
+          lineSmooth: Chartist.Interpolation.cardinal({
+              tension: 0
+          }),
+          low: 0,
+          high: 20, // creative tim: we recommend you to set the high sa the biggest value + something for a better
+          // look
+          chartPadding: { top: 0, right: 0, bottom: 0, left: 0}
+      };
+
+     const completedTasksChart = new Chartist.Line('#completedTasksChart', dataCompletedTasksChart,
+      optionsCompletedTasksChart);
+
+     this.startAnimationForLineChart(completedTasksChart);
+
+      /* ----------==========     Emails Subscription Chart initialization    ==========---------- */
+
+      const dataWebsiteViewsChart = {
+        labels: ['J', 'F', 'M', 'A', 'M', 'J', 'J', 'A', 'S', 'O', 'N', 'D'],
+        series: [
+          [54200, 94300, 32000, 78000, 53000, 85300, 32600, 43400, 56800, 61000, 35600, 75521]
+
+        ]
+      };
+      const optionsWebsiteViewsChart = {
+          axisX: {
+              showGrid: false
+          },
+          low: 0,
+          high: 100000,
+          chartPadding: { top: 0, right: 5, bottom: 0, left: 0}
+      };
+      const responsiveOptions: any = [
+        ['screen and (max-width: 640px)', {
+          seriesBarDistance: 5,
+          axisX: {
+            labelInterpolationFnc: function (value) {
+              return value[0];
+            }
+          }
+        }]
+      ];
+      const websiteViewsChart = new Chartist.Bar('#websiteViewsChart', dataWebsiteViewsChart, optionsWebsiteViewsChart, responsiveOptions);
+
+      this.startAnimationForBarChart(websiteViewsChart);
+
+      $('#worldMap').vectorMap({
+        map: 'world_en',
+        backgroundColor: 'transparent',
+         borderColor: '#818181',
+         borderOpacity: 0.25,
+         borderWidth: 1,
+         color: '#b3b3b3',
+         enableZoom: true,
+         hoverColor: '#eee',
+         hoverOpacity: null,
+         normalizeFunction: 'linear',
+         scaleColors: ['#b6d6ff', '#005ace'],
+         selectedColor: '#c9dfaf',
+         selectedRegions: null,
+         showTooltip: true,
+         onRegionClick: function(element, code, region)
+         {
+             var message = 'You clicked "'
+                 + region
+                 + '" which has the code: '
+                 + code.toUpperCase();
+
+             alert(message);
+         }
+      });
+   }
+   ngAfterViewInit() {
+       const breakCards = true;
+       if (breakCards === true) {
+           // We break the cards headers if there is too much stress on them :-)
+           $('[data-header-animation="true"]').each(function(){
+               const $fix_button = $(this);
+               const $card = $(this).parent('.card');
+               $card.find('.fix-broken-card').click(function(){
+                   const $header = $(this).parent().parent().siblings('.card-header, .card-image');
+                   $header.removeClass('hinge').addClass('fadeInDown');
+
+                   $card.attr('data-count', 0);
+
+                   setTimeout(function(){
+                       $header.removeClass('fadeInDown animate');
+                   }, 480);
+               });
+
+               $card.mouseenter(function(){
+                   const $this = $(this);
+                   const hover_count = parseInt($this.attr('data-count'), 10) + 1 || 0;
+                   $this.attr('data-count', hover_count);
+                   if (hover_count >= 20) {
+                       $(this).children('.card-header, .card-image').addClass('hinge animated');
+                   }
+               });
+           });
+       }
+   }
+}
---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,92 @@
+import { Component, OnInit } from '@angular/core';
+import PerfectScrollbar from 'perfect-scrollbar';
+
+declare var $: any;
+
+//Metadata
+export interface RouteInfo {
+    path: string;
+    title: string;
+    type: string;
+    icontype: string;
+    collapse?: string;
+    children?: ChildrenItems[];
+}
+
+export interface ChildrenItems {
+    path: string;
+    title: string;
+    ab: string;
+    type?: string;
+    icontype?: string;
+}
+
+//Menu Items
+export const ROUTES: RouteInfo[] = [{
+        path: '/dashboard',
+        title: 'Dashboard',
+        type: 'link',
+        icontype: 'dashboard'
+    },{
+        path: '/lancamentos',
+        title: 'Lançamentos',
+        type: 'sub',
+        icontype: 'construction',
+        collapse: 'components',
+        children: [
+            {path: 'lote', title: 'Em Lote', ab:'LT', icontype:'apps'},
+            {path: 'individual', title: 'Individual', ab:'IND', icontype:'content_paste'}
+        ]
+    },{
+        path: '/consultas',
+        title: 'Consultas',
+        type: 'link',
+        icontype: 'search'
+
+    },{
+        path: '/configuracao',
+        title: 'Configuração',
+        type: 'sub',
+        icontype: 'settings',
+        collapse: 'tables',
+        children: [
+            {path: 'regular', title: 'Configurar Permissões', ab:'RT', icontype:'manage_accounts'},
+            {path: 'extended', title: 'Unidade x CIF', ab:'ET', icontype:'published_with_changes' }
+        ]
+    }
+];
+@Component({
+    selector: 'app-sidebar-cmp',
+    templateUrl: 'sidebar.component.html',
+})
+
+export class SidebarComponent implements OnInit {
+    public menuItems: any[];
+    ps: any;
+    isMobileMenu() {
+        if ($(window).width() > 991) {
+            return false;
+        }
+        return true;
+    };
+
+    ngOnInit() {
+        this.menuItems = ROUTES.filter(menuItem => menuItem);
+        if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
+            const elemSidebar = <HTMLElement>document.querySelector('.sidebar .sidebar-wrapper');
+            this.ps = new PerfectScrollbar(elemSidebar);
+        }
+    }
+    updatePS(): void  {
+        if (window.matchMedia(`(min-width: 960px)`).matches && !this.isMac()) {
+            this.ps.update();
+        }
+    }
+    isMac(): boolean {
+        let bool = false;
+        if (navigator.platform.toUpperCase().indexOf('MAC') >= 0 || navigator.platform.toUpperCase().indexOf('IPAD') >= 0) {
+            bool = true;
+        }
+        return bool;
+    }
+}
